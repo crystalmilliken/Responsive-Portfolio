12f5ac0f69eb1512190bbf3df2e59210fd0ef385
crystalmilliken <crystal@millikenconsulting.com>
2017-12-16 05:32:54 -0500

    last changes

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+
+function changeSocialPic(e){
+    document.getElementById(e).setAttribute("src", `assets/images/${e}Hover.png`);
+}
+function changeBack(e){
+    document.getElementById(e).setAttribute("src", `assets/images/${e}.png`);
+}
